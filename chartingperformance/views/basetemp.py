9ba4733e2fadecf4ea1d9fa2b48d720364bc38dd
Larry Burks <lburks@gmail.com>
2017-01-14 08:22:08 -0500

    views broken into more manageable modules

---
+++ chartingperformance/views/basetemp.py
@@ -0,0 +1,100 @@
+""" ViewBasetemp class """
+# pylint: disable=no-member
+from chartingperformance import db_session
+from chartingperformance.views.view import View
+
+from flask import jsonify
+
+from sqlalchemy.sql import text
+
+class Basetemp(View):
+    """ Basetemp analysis view query and response methods. """
+
+    def __init__(self, args, house_id):
+
+        super(Basetemp, self).__init__(args)
+
+        if self.success:
+            self.get_items(house_id)
+
+    def get_items(self, house_id):
+        """ Get and store points (primarily hdd and ashp values) from database. """
+
+        items = db_session.query("hdd", "ashp", "temperature", "date", "solar")
+
+        if self.args['start'] is not None and self.args['end'] is not None:
+            date_range = " AND date BETWEEN :start AND :end "
+
+        elif self.args['start'] is not None:
+            date_range = " AND date >= :start "
+
+        elif self.args['end'] is not None:
+            date_range = " AND date < :end "
+
+        elif self.args['start'] is None and self.args['end'] is None:
+            date_range = ""
+
+        if 'hour' in self.args['interval']:
+            sql = """SELECT e.date AS 'date',
+                     t.hdd AS 'hdd',
+                     e.ashp/1000.0 AS 'ashp',
+                     t.temperature AS 'temperature',
+                     e.solar/1000.0 AS 'solar'
+                  """
+
+        else:
+            sql = """SELECT e.date AS 'date',
+                     SUM(t.hdd) AS 'hdd',
+                     SUM(e.ashp)/1000.0 AS 'ashp',
+                     AVG(t.temperature) AS 'temperature',
+                     SUM(e.solar)/1000.0 AS 'solar'
+                  """
+
+        sql = sql + \
+        """FROM (SELECT date, solar, ashp
+           FROM energy_hourly
+           WHERE house_id = :house_id
+           AND solar > -500
+           AND ashp > 50 %s) e
+           LEFT JOIN
+           (SELECT date, temperature, (:base - temperature) / 24.0 AS 'hdd'
+           FROM temperature_hourly
+           WHERE house_id = :house_id
+           AND device_id = 0
+           AND temperature <= :base %s) t ON e.date = t.date
+           WHERE t.date = e.date
+        """ % (date_range, date_range)
+
+        if self.args['interval'] == 'year':
+            sql = sql + "GROUP BY YEAR(t.date)"
+
+        if self.args['interval'] == 'month':
+            sql = sql + "GROUP BY YEAR(t.date), MONTH(t.date)"
+
+        if self.args['interval'] == 'day':
+            sql = sql + "GROUP BY CAST(t.date AS DATE)"
+
+        items = items.from_statement(text(sql))
+        items = items.params(house_id=house_id,
+                             base=self.args['base'],
+                             start=self.args['start'],
+                             end=self.args['end']).all()
+
+        self.json_items = []
+        for item in items:
+            data = {'date': self.format_date(item.date),
+                    'solar': item.solar,
+                    'ashp': item.ashp,
+                    'temperature': item.temperature,
+                    'hdd': item.hdd}
+            self.json_items.append(data)
+
+    def get_response(self):
+        """ Return response in json format. """
+        if not self.success:
+            return jsonify(self.error)
+
+        return jsonify(view='heat',
+                       base=self.args['base'],
+                       interval=self.args['interval'],
+                       points=self.json_items)
---
+++ chartingperformance/views/generation.py
@@ -0,0 +1,121 @@
+""" ViewGeneration class """
+# pylint: disable=no-member
+from chartingperformance import db_session
+from chartingperformance.views.view import View
+
+from chartingperformance.models import EnergyHourly
+from chartingperformance.models import EnergyDaily
+from chartingperformance.models import EnergyMonthly
+from chartingperformance.models import EstimatedMonthly
+
+from flask import jsonify
+
+from sqlalchemy import func
+from sqlalchemy.sql import label, and_
+
+class Generation(View):
+    """ Genration view query and response methods. """
+
+    def __init__(self, args, house_id):
+
+        super(Generation, self).__init__(args)
+
+        if self.success:
+            self.get_fields(house_id)
+            self.get_totals(house_id)
+            self.get_items()
+
+    def get_fields(self, house_id):
+        """ Get and store additional generation fields from database. """
+
+        tables = [EnergyHourly, EnergyDaily]
+        self.max_solar = []
+        for table in tables:
+            self.base_query = db_session.query(label('max_solar',
+                                                     func.min(table.solar))).\
+                filter(table.house_id == house_id) # nested query
+
+            sub_query = self.filter_query_by_date_range(table)
+
+            max_solar_query = db_session.query(table.date,
+                                               label('max_solar', table.solar)).\
+                filter(table.solar == sub_query).first()
+
+            self.max_solar.append({'date': str(max_solar_query.date),
+                                   'solar': max_solar_query.max_solar})
+
+    def get_totals(self, house_id):
+        """ Get and store totals from database. """
+
+        if ('year' in self.args['interval']) or ('month' in self.args['interval']):
+            self.get_totals_year_month(house_id)
+
+        elif ('day' in self.args['interval']) or ('hour' in self.args['interval']):
+            self.get_totals_day_hour(house_id)
+
+    def get_totals_year_month(self, house_id):
+        """ Get and store yearly or monthly totals. """
+
+        self.base_query = db_session.query(EnergyMonthly.date,
+                                           label('sum_actual',
+                                                 func.sum(EnergyMonthly.solar)),
+                                           label('sum_estimated',
+                                                 func.sum(EstimatedMonthly.solar))).\
+            outerjoin(EstimatedMonthly,
+                      and_(EnergyMonthly.date == EstimatedMonthly.date,
+                           EnergyMonthly.house_id == EstimatedMonthly.house_id)).\
+            filter(EnergyMonthly.house_id == house_id)
+
+        self.filter_query_by_date_range(EnergyMonthly)
+
+        totals = self.base_query.one()
+
+        self.json_totals = {'actual': totals.sum_actual,
+                            'estimated': totals.sum_estimated}
+
+    def get_totals_day_hour(self, house_id):
+        """ Get and store daily or hourly totals. """
+
+        self.base_query = db_session.query(EnergyHourly.date,
+                                           label('sum_actual',
+                                                 func.sum(EnergyHourly.solar)/1000)).\
+            filter(EnergyHourly.house_id == house_id)
+
+        self.filter_query_by_date_range(EnergyHourly)
+
+        totals = self.base_query.one()
+
+        self.json_totals = {'actual': totals.sum_actual}
+
+    def get_items(self):
+        """ Get and store rows from database. """
+
+        self.json_items = []
+
+        if ('year' in self.args['interval']) or ('month' in self.args['interval']):
+            items = self.group_query_by_interval(EnergyMonthly)
+            for item in items:
+                data = {'date': str(item.date),
+                        'actual': item.sum_actual,
+                        'estimated': item.sum_estimated}
+                self.json_items.append(data)
+
+        elif ('day' in self.args['interval']) or ('hour' in self.args['interval']):
+            items = self.group_query_by_interval(EnergyHourly)
+            for item in items:
+                data = {'date': self.format_date(item.date),
+                        'actual': item.sum_actual}
+                self.json_items.append(data)
+
+    def get_response(self):
+        """ Return response in json format. """
+
+        if not self.success:
+            return jsonify(self.error)
+
+        return jsonify(view='generation',
+                       interval=self.args['interval'],
+                       max_solar_hour=self.max_solar[0],
+                       max_solar_day=self.max_solar[1],
+                       totals=self.json_totals,
+                       items=self.json_items)
